079ce7ac59612f86ca999c953548b2dc16831935
Prakhar <prakhar.bits14@gmail.com>
2020-03-27 14:55:22 +0530

    Patch | Express-session

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const express = require("express"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
     User = require("./models/user")
+    methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/smart_home", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
 
 app.use(require("express-session")({
     secret: "Welcome to the future",
@@ -18,12 +21,23 @@ app.use(require("express-session")({
     saveUninitialized: false
 }));
 
+app.use(require("express-session")({
+    secret: "Welcome to the future",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.use(authRoutes);
 app.use("/devices", deviceRoutes);
